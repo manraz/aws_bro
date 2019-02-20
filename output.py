aa43dc734ae5b36a2694595ac111ade12b2df60e
Manuel <manuelrazvan@gmail.com>
2019-02-20 14:00:18 +0000

    Update output.py

--- output.py
+++ output.py
@@ -6,34 +6,29 @@ Description: Script to run the app and render all log values into html
  common/least domain ...
 '''
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, flash, redirect, request, session, abort
 from getLst import *
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 app = Flask(__name__)
 
-
-# initiate the flask
-script = Flask(__name__)
-
-
+folder = os.path.abspath(os.path.dirname(__file__))
+# ze_path = folder + '/20110919/http.log'
 # route for homepage
 @app.route('/')
 def homepage():
-
-    # change to directory
-    folder = os.path.abspath(os.path.dirname(__file__))
-    ze_path = folder + '/20140315/http.log'
-
+    ze_path = os.path.join(folder, '20110919/http.log')
     # get fields and values from all files
-    nestLst, fieldsLst,  ipLst, mostDomLst = getNestLst(ze_path)
+    nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
 
     # dictionary for geolocation records
     recordDict = {}
-    setipLst = set(ipLst)
     # returns dicitonary with records for ip with more than 2 occurences
-    for ip in setipLst:
+    for ip in ipLstSet:
         if ipLst.count(ip) > 2:
             record = ipLocator(ip)
             recordDict[ip] = record
@@ -42,13 +37,13 @@ def homepage():
         agentList.append(agent[11])
     setAgentLst = set(agentList)
 
-    # dictionary for user agent and occurences count
+    # dictionary for user agent and occurrences count
     agentDict = {}
     for agent in setAgentLst:
         if agentList.count(agent) >= 1:
             agentCount = agentList.count(agent)
             agentDict[agent] = agentCount
-    # most used domain dictionary with counter for occurences
+    # most used domain dictionary with counter for occurrences
     domainDict = {}
     for domain in mostDomLst:
         if mostDomLst.count(domain) >= 1:
@@ -61,70 +56,84 @@ def homepage():
         privIPLst.append(ip[2])
     setPrivIP = set(privIPLst)
 
+    ipnumber = []
+    hits = []
+
+    def build_pie_most_ip():
+        for ip in ipLstSet:
+            if ipLst.count(ip) >= 1:
+                ipnumber.append(ip)
+                hits.append(ipLst.count(ip))
+
+        if hits.__len__() < 10:
+            plt.axis('equal')
+            plt.pie(hits, labels=ipnumber, autopct='%1.1f%%', shadow=True, startangle=140)
+            plt.title('Most used IP\'s')
+            plt.savefig('static/images/most_priv_ip.png')
+        else:
+            y_pos = np.arange(len(ipnumber))
+            width = 0.5
+            plt.figure(figsize=(10, 4))
+            plt.barh(y_pos, hits, width, align='center', alpha=1)
+            plt.yticks(y_pos, ipnumber)
+            plt.xlabel('Count')
+            plt.title('Most used IP\'s')
+            plt.savefig('static/images/most_priv_ip.png')
+    build_pie_most_ip()
+
     return render_template("homepage.html", nestLst=nestLst,
                            fieldsLst=fieldsLst, ipLst=ipLst, ipLstSet=ipLstSet,
                            record=record, recordDict=recordDict,
-                           agentDict=agentDict, domainDict=domainDict, setPrivIP=setPrivIP, privIPLst=privIPLst)
+                           agentDict=agentDict, domainDict=domainDict, setPrivIP=setPrivIP, privIPLst=privIPLst,
+                           name='Most Active Public IP', url='/../static/images/most_priv_ip.png')
 
 
 # route for page where conn.log is analyzed
 @app.route('/connpage/')
 def connpage():
-
-    # change to directory
-    folder = os.path.abspath(os.path.dirname(__file__))
-    ze_path = folder + '/20140315/conn.log'
-
     # get fields and values from all files
-    nestLst, fieldsLst,  ipLst,  mostDomLst = getNestLst(ze_path)
+    ze_path = os.path.join(folder, '20110919/conn.log')
+    nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
     # dictionary for ip with most occurences for geolocation
     recordDict = {}
-    setipLst = set(ipLst)
-    for ip in setipLst:
+    for ip in ipLstSet:
         if ipLst.count(ip) > 2:
             record = ipLocator(ip)
             recordDict[ip] = record
-    return render_template("connpage.html", nestLst=nestLst, fieldsLst=fieldsLst, ipLst=ipLst, ipLstSet=ipLstSet, record=record, recordDict=recordDict)
+    return render_template("connpage.html", nestLst=nestLst, fieldsLst=fieldsLst, ipLst=ipLst, ipLstSet=ipLstSet,
+                           record=record, recordDict=recordDict)
 
 
 # route for page whre dns.log is analyzed
 @app.route('/dnspage/')
 def dnspage():
-
-    # upload folder
-    folder = os.path.abspath(os.path.dirname(__file__))
-    ze_path = folder + '/20140315/dns.log'
-
     # get fields and values from all files
-    nestLst, fieldsLst,  ipLst,  mostDomLst = getNestLst(ze_path)
+    ze_path = os.path.join(folder, '20110919/dns.log')
+    nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
     # dictionary for ip with most occurences for geolocation
     recordDict = {}
-    setipLst = set(ipLst)
-    for ip in setipLst:
+    for ip in ipLstSet:
         if ipLst.count(ip) >= 2:
             record = ipLocator(ip)
             recordDict[ip] = record
-    return render_template("dnspage.html", nestLst=nestLst, fieldsLst=fieldsLst, ipLst=ipLst, ipLstSet=ipLstSet, record=record, recordDict=recordDict)
+    return render_template("dnspage.html", nestLst=nestLst, fieldsLst=fieldsLst, ipLst=ipLst, ipLstSet=ipLstSet,
+                           record=record, recordDict=recordDict)
+
 
 # route for page whre dns.log is analyzed
 
 
 @app.route('/dhcppage/')
 def dhcppage():
-
-    # upload folder
-    folder = os.path.abspath(os.path.dirname(__file__))
-    ze_path = folder + '/20140315/dhcp.log'
-
     # get fields and values from all files
-    nestLst, fieldsLst,  ipLst, mostDomLst = getNestLst(ze_path)
+    ze_path = os.path.join(folder, '20110919/dhcp.log')
+    nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
     # dictionary for ip with most occurrences for geolocation
     recordDict = {}
-    setipLst = set(ipLst)
-    for ip in setipLst:
+    for ip in ipLstSet:
         if ipLst.count(ip) >= 2:
             record = ipLocator(ip)
             recordDict[ip] = record
@@ -136,7 +145,8 @@ def dhcppage():
         company = mac_info(nic[6])
     setNicList = set(nicList)
 
-    return render_template("dhcppage.html", nestLst=nestLst, fieldsLst=fieldsLst, setNicList=setNicList, company=company)
+    return render_template("dhcppage.html", nestLst=nestLst, fieldsLst=fieldsLst, setNicList=setNicList,
+                           company=company)
 
 
 #  funny about page
