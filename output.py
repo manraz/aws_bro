a7d02ebcbb5a2d19b9d537ba976c6d87a5f8ba5b
Manuel <manuelrazvan@gmail.com>
2019-02-27 20:33:40 +0000

    Update output.py

--- output.py
+++ output.py
@@ -8,22 +8,33 @@ Description: Script to run the app and render all log values into html
 
 from flask import Flask, render_template, flash, redirect, request, session, abort
 from getLst import *
+from build_graph import *
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 app = Flask(__name__)
 
 folder = os.path.abspath(os.path.dirname(__file__))
-# ze_path = folder + '/20110919/http.log'
+
+
+
 # route for homepage
 @app.route('/')
 def homepage():
-    ze_path = os.path.join(folder, '20110919/http.log')
+    ze_path = os.path.join(folder, '20140315/http.log')
     # get fields and values from all files
     nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
+
+    ipLstSet = set(ipLst)
+    ipnumber = []
+    hits = []
+
+    # method to build graph with ip and number of time ip was logged
+    for ip in ipLstSet:
+        if ipLst.count(ip) > 1:
+            ipnumber.append(ip)
+            hits.append(ipLst.count(ip))
 
     # dictionary for geolocation records
     recordDict = {}
@@ -56,43 +67,18 @@ def homepage():
         privIPLst.append(ip[2])
     setPrivIP = set(privIPLst)
 
-    ipnumber = []
-    hits = []
-
-    def build_pie_most_ip():
-        for ip in ipLstSet:
-            if ipLst.count(ip) >= 1:
-                ipnumber.append(ip)
-                hits.append(ipLst.count(ip))
-
-        if hits.__len__() < 10:
-            plt.axis('equal')
-            plt.pie(hits, labels=ipnumber, autopct='%1.1f%%', shadow=True, startangle=140)
-            plt.title('Most used IP\'s')
-            plt.savefig('static/images/most_priv_ip.png')
-        else:
-            y_pos = np.arange(len(ipnumber))
-            width = 0.5
-            plt.figure(figsize=(10, 4))
-            plt.barh(y_pos, hits, width, align='center', alpha=1)
-            plt.yticks(y_pos, ipnumber)
-            plt.xlabel('Count')
-            plt.title('Most used IP\'s')
-            plt.savefig('static/images/most_priv_ip.png')
-    build_pie_most_ip()
-
     return render_template("homepage.html", nestLst=nestLst,
                            fieldsLst=fieldsLst, ipLst=ipLst, ipLstSet=ipLstSet,
                            record=record, recordDict=recordDict,
                            agentDict=agentDict, domainDict=domainDict, setPrivIP=setPrivIP, privIPLst=privIPLst,
-                           name='Most Active Public IP', url='/../static/images/most_priv_ip.png')
+                           name='Most Active Public IP', graph=build_graph(hits, ipnumber),folder=folder)
 
 
 # route for page where conn.log is analyzed
 @app.route('/connpage/')
 def connpage():
     # get fields and values from all files
-    ze_path = os.path.join(folder, '20110919/conn.log')
+    ze_path = os.path.join(folder, '20140315/conn.log')
     nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
     # dictionary for ip with most occurences for geolocation
@@ -109,7 +95,7 @@ def connpage():
 @app.route('/dnspage/')
 def dnspage():
     # get fields and values from all files
-    ze_path = os.path.join(folder, '20110919/dns.log')
+    ze_path = os.path.join(folder, '20140315/dns.log')
     nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
     # dictionary for ip with most occurences for geolocation
@@ -128,7 +114,7 @@ def dnspage():
 @app.route('/dhcppage/')
 def dhcppage():
     # get fields and values from all files
-    ze_path = os.path.join(folder, '20110919/dhcp.log')
+    ze_path = os.path.join(folder, '20140315/dhcp.log')
     nestLst, fieldsLst, ipLst, mostDomLst = getNestLst(ze_path)
     ipLstSet = set(ipLst)
     # dictionary for ip with most occurrences for geolocation
